96838d91a9e00dbb8c959918fbbe46c4e7ff5006
Rich Leland <rich@richleland.com>
2011-06-17 09:21:52 -0400

    added basic js logic to handle mouse events

--- code/game.js
+++ code/game.js
@@ -1,3 +1,19 @@
 $(function(){
+  var board = $("#board");
+  var squares = board.find("td");
 
+  squares.bind("mouseenter", function(e){
+    var square = $(this);
+    square.addClass("highlight");
+  });
+
+  squares.bind("mouseleave", function(e){
+    var square = $(this);
+    square.removeClass("highlight");
+  });
+
+  squares.bind("click", function(e){
+    var square = $(this);
+    console.log("clicked square " + square.attr("id"));
+  });
 });
