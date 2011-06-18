4889c18de328f1cd83b30bd2d1c728ed40444e74
Rich Leland <rich@richleland.com>
2011-06-18 15:32:54 -0400

    gonna need that minimax thang. set up pseudocode and evaluation board.

---
+++ code/minimax.js
@@ -0,0 +1,57 @@
+$(function(){
+  var alphaBeta = function(state, depth, alpha, beta) {
+    /* pseudocode:
+       
+      get all valid moves
+      if (depth is 0 or there are no valid moves or the game is over)
+        return the score from examineState(state)
+      for each move in available moves
+        pretend to make the move, getting the new state of the board if it had that move
+        get value of recursive run of alphaBeta(new state, depth -1, -beta, -alpha)
+        if value is greater than alpha
+          set alpha to value
+        if alpha is greater than or equal to beta
+          break, pruning this branch
+      return the alpha value
+
+     */
+  };
+
+  var getBestMove = function(state) {
+    /* pseudocode
+      
+      set constants for alpha (-1000, game lost) and beta (1000, game won)
+      keep track of the best move and best rank
+      get all valid moves
+      for each move in available moves
+        pretend to make the move, getting the new state of the board if it had that move
+        rank the move using alphaBeta(new state, depth, alpha, beta)
+        set the best move and the best rank
+        if the new rank (the result of alphaBeta) is greater than best rank
+          set best move to current move
+          set best rank to rank (the result of alphaBeta)
+      return the best move
+
+    */
+  };
+
+  var examineState = function(state) {
+    /* pseudocode
+      
+      set score to 0
+      if the game is over
+        return 1000 if won, -1000 if lost
+      calculate how many fields we have
+      calculate how many fields our opponent has
+      increment score by fields we have
+      decrement score by fields opponent has
+      return score
+    */
+  };
+
+  var getValidMoves = function(state) {
+    // grab all valid moves on the board supplied state
+  };
+
+});
+
