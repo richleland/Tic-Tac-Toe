ac598682c7a2e4efe457d3041fbd90ac96fc55d5
Rich Leland <rich@richleland.com>
2011-06-19 21:27:51 -0400

    Respond to board clicks, showing X or O as appropriate.

--- code/game.js
+++ code/game.js
@@ -1,2 +1,24 @@
 $(function(){
+  var possibleWins = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+  var board = $("#board");
+  var squares = board.find("td");
+  var info = $("#info");
+  var computersTurn = false;
+  var X = "X";
+  var O = "O";
+
+  squares.click(function() {
+    var square = $(this);
+    var letter = computersTurn ? O : X;
+    if(square.text() == "") {
+      square.text(letter);
+    } else {
+      info.text("That space is occupied by an " + square.text() + ".");
+    }
+    computersTurn = !computersTurn;
+  });
 });
