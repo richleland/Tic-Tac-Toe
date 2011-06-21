6a8efc3dcd402b5b432f1cc7ca7e24f669593ac2
Rich Leland <rich@richleland.com>
2011-06-20 20:22:43 -0400

    Allow options to be passed to plugin.

--- code/jquery.tictac.js
+++ code/jquery.tictac.js
@@ -1,7 +1,12 @@
 (function($) {
-  $.fn.tictac = function() {
+  $.fn.tictac = function(options) {
+    var settings = {};
+
     return this.each(function() {
-      //
+      // apply custom options if supplied
+      if(options) {
+        $.extend(settings, options);
+      }
     });
   };
 })(jQuery);
